9ce07d2ebc6b38af1e6a540bd7c5d2f7262aa3c2
Prathamdas3 <prathamdas003@gmail.com>
2023-09-02 18:28:20 +0530

    (style): started the landing page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+
+import LandingPage from './Pages/landingPage';
+import RecipesPage from './Pages/RecipesPage';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
-      <div>hi</div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route exact path="/recipes" element={<RecipesPage />} />
+      </Routes>
     </>
   );
 }
---
+++ src/Pages/RecipesPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RecipesPage = () => {
+  return <div>This is recipes page</div>;
+};
+
+export default RecipesPage;
