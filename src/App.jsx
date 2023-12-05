b99c0a176d508426a77cc95f871139505d060b1a
Pratham Das <116100433+Prathamdas3@users.noreply.github.com>
2023-12-05 17:31:15 +0530

    (chore): added code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { Routes, Route } from 'react-router-dom';
-import LandingPage from './Pages/landingPage';
-import RecipesPage from './Pages/RecipesPage';
-import DetailsPage from './Pages/DetailsPage';
-import FromPage from './Pages/FromPage';
-import NotFoundPage from './Pages/NotFoundPage';
+import React, { lazy, Suspense } from 'react'
+import { Routes, Route } from 'react-router-dom'
+const LandingPage = lazy(() => import('./Pages/landingPage'))
+const RecipesPage = lazy(() => import('./Pages/RecipesPage'))
+const DetailsPage = lazy(() => import('./Pages/DetailsPage'))
+const FromPage = lazy(() => import('./Pages/FromPage'))
+const NotFoundPage = lazy(() => import('./Pages/NotFoundPage'))
 
 export default function App() {
   return (
     <>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route exact path="/recipes" element={<RecipesPage />} />
-        <Route exact path="/recipes/:id" element={<DetailsPage />} />
-        <Route exact path="/xyz/form" element={<FromPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/recipes" element={<RecipesPage />} />
+          <Route exact path="/recipes/:id" element={<DetailsPage />} />
+          <Route exact path="/xyz/form" element={<FromPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </>
-  );
+  )
 }
